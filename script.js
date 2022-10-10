09907d820490b368908cd2be2b19ee7f6f7663de
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2022-10-10 20:49:53 +0600

    fetch requst logic  for all list of students is done

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-const allStudents = document.getElementById('allStudents');
-const studentData = document.getElementById('student_data');
+let studentData = document.createElement('tbody');
+let div =document.getElementById('student_data')
 
 const renderAllStudents = async() => {
     const result = await fetch('https://hp-api.herokuapp.com/api/characters/students')
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+        const students = data
+        .map(student=>{
+            return `
+             <tr>
+                <th>${student.name}</th>
+                <th>${student.dateOfBirth}</th>
+                <th>${student.house}</th>
+                <th>${student.wizard}</th>
+                <th>${student.ancestry}</th>
+                <th>${student.hogwartsStudent}</th>
+            </tr>`;
+        })
+
+          studentData.innerHTML = students;
+          div.appendChild(studentData);
+    });
 }
 
-renderAllStudents();
+
+let allStudents = document.getElementById('fetch_all');
+allStudents.addEventListener("click", renderAllStudents);
