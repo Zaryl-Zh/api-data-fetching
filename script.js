d0950a5e419ed057c0088a04dc0268784f2ffc54
Zaryl-Zh <zaryl.abdrazakova@gmail.com>
2022-10-11 18:19:06 +0600

    fetch request logic according to house name is done

--- script.js
+++ script.js
@@ -1,28 +1,65 @@
 let studentData = document.createElement('tbody');
-let div =document.getElementById('student_data')
+let table=document.getElementById('student_data')
 
-const renderAllStudents = async() => {
-    const result = await fetch('https://hp-api.herokuapp.com/api/characters/students')
-    .then((response) => response.json())
-    .then((data) => {
-        const students = data
-        .map(student=>{
-            return `
-             <tr>
-                <th>${student.name}</th>
-                <th>${student.dateOfBirth}</th>
-                <th>${student.house}</th>
-                <th>${student.wizard}</th>
-                <th>${student.ancestry}</th>
-                <th>${student.hogwartsStudent}</th>
-            </tr>`;
-        })
-
-          studentData.innerHTML = students;
-          div.appendChild(studentData);
-    });
+const htmlTable = (value) => {
+     return `
+<tr>
+   <th>${value.name}</th>
+   <th>${value.dateOfBirth}</th>
+   <th>${value.house}</th>
+   <th>${value.wizard}</th>
+   <th>${value.ancestry}</th>
+   <th>${value.hogwartsStudent}</th>
+</tr>`
 }
 
+const URL = 'https://hp-api.herokuapp.com/api/characters/students'
 
-let allStudents = document.getElementById('fetch_all');
+ const fetchRequest = async(value) => {
+         await fetch(URL)
+        .then((response) => response.json())
+        .then((data) => {
+            const students = data
+            .map(student=>{
+                if( value.length > 0 && student.house == value){
+                    return htmlTable(student);
+                } 
+                else if( value.length == 0) {
+                    return htmlTable(student)
+                }
+                    
+            }).join('');
+    
+              studentData.innerHTML = students;
+              table.appendChild(studentData);
+        });
+    }
+
+const renderAllStudents = () => {
+    fetchRequest('')
+};
+const renderGryffindor = () =>{
+    fetchRequest('Gryffindor')
+}
+const renderSlytherin = () => {
+    fetchRequest('Slytherin')
+}
+const renderHufflepuff = () => { 
+    fetchRequest('Hufflepuff') 
+}
+const renderRavenclaw = () => { 
+    fetchRequest('Ravenclaw')
+}
+
+const allStudents = document.getElementById('fetch_all');
+const gryffindor = document.getElementById('fetch_gryffindor');
+const slytherin = document.getElementById('fetch_slytherin');
+const hufflepuff = document.getElementById('fetch_hufflepuff');
+const ravenclaw = document.getElementById('fetch_ravenclaw');
+
+
 allStudents.addEventListener("click", renderAllStudents);
+gryffindor.addEventListener("click", renderGryffindor);
+slytherin.addEventListener("click", renderSlytherin);
+hufflepuff.addEventListener("click", renderHufflepuff);
+ravenclaw.addEventListener("click", renderRavenclaw);
